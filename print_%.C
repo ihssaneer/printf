da6f5acb1a48b22a1a13fbaeb55836fbed18800a
kouar jihane <kouarjihane@gmail.com>
2023-11-13 12:08:45 -0800

    printf

---
+++ print_%.C
@@ -0,0 +1,12 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_% - PRINT %
+ * Return: character to print
+ */
+int print_%(void)
+{_putchar(37);
+	return (1);
+}
+
---
+++ print_d.c
@@ -0,0 +1,79 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+if (num / 10 != 0)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + '0');
+}
+
+/**
+ * print_int - print integer in d conversion
+ * @args: argument
+ * Return: length
+ */
+
+int print_int (va_list args)
+{
+	int n;
+	int num;
+		
+	n = va_arg(args, int);
+
+if (n < 0)
+	_putchar('-');
+i++;
+num = - n;
+
+else num = n;
+
+while (num / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+ i++;
+  return (i);
+}
+
+/**
+ * print_d - print integer in d conversion
+ * @args: argument
+ * Return: length
+ */
+
+int print_d (va_list args)
+{
+        int n;
+        int num;
+
+        n = va_arg(args, int);
+
+if (n < 0)
+        _putchar('-');
+i++;
+num = - n;
+
+else num = n;
+
+while (num / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+ i++;
+  return (i);
+}
