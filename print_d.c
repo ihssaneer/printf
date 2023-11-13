88a8196f2e8bd5959af208ea614c31ba4e26542c
kouar jihane <kouarjihane@gmail.com>
2023-11-13 12:18:21 -0800

    jih

--- print_d.c
+++ print_d.c
@@ -11,6 +11,7 @@ unsigned int num = n;
 if (n < 0)
 {
 _putchar('-');
+ i++;
 num = -n;
 }
 if (num / 10 != 0)
@@ -18,6 +19,7 @@ if (num / 10 != 0)
 print_number(num / 10);
 }
 _putchar((num % 10) + '0');
+i++;
 }
 
 /**
@@ -27,25 +29,10 @@ _putchar((num % 10) + '0');
  */
 
 int print_int (va_list args)
-{
-	int n;
-	int num;
-		
+{	
 	n = va_arg(args, int);
 
-if (n < 0)
-	_putchar('-');
-i++;
-num = - n;
-
-else num = n;
-
-while (num / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
- i++;
+print_number(n);
   return (i);
 }
 
