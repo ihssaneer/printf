8775bb9001941bffc34674745a886a800627fccf
ihssaneer <errajy.ihssane@gmail.com>
2023-11-14 18:03:29 +0100

    Fixing print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,17 +1,16 @@
 #include"main.h"
-#include<stdio.h>
+
 /**
  * print_binary - print binary of numer
- * @args: argument 
- * Return: binary 
+ * @args: argument
+ * Return: binary
  */
 
 int print_binary(va_list args)
 {
 	unsigned int number = va_arg(args, unsigned int);
-	int len = 0, i;
-	char c;
-	char array[25];
+	int len = 0, i = 0;
+	char c, array[20];
 
 	if (number == 0)
 	{
@@ -20,33 +19,21 @@ int print_binary(va_list args)
 	}
 	if (number > 0)
 	{
-		for (i = 0 ; i < 20 ; i++)
+		while ((number / 2) != 0)
 		{
-			if (number / 2 != 0)
-			{
-				array[i] = (number % 2) + '0';
-				number = number / 2;
-			}
-			if ((number / 2) == 0)
-			{	
-				array[i] = (number % 2) + '0';
-				break;
-			}
-		
-			len += write(1, array, i);
+			array[i] = (number % 2) + '0';
+			number = number / 2;
+			i++;
 		}
+		if (number / 2 == 0)
+			array[i] = (number % 2) + '0';
+		i++;
+		array[i] = '\0';
+		for (; i >= 0; i--)
+		{
+			c = array[i];
+			len += write(1, &c, 1);
+		}
+	}
 	return (len);
 }
-``while (number / 2 != 0)
-                {
-                        c = (number % 2) + '0';
-                        len += write(1, &c, 1);
-                        number = number / 2;
-                }
-                if (number / 2 == 0)
-                {
-                        c = (number % 2) + '0';
-                        len += write(1, &c, 1);
-                }
-        }
--- INSERT --               ``
